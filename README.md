7e5930a09c23788b1bfb9e7f52c24416ce1986e7
Faizaditya-oss <faizaditya750@gmail.com>
2026-09-24 20:18:13 +0700

    WHAT ?

---
+++ README.md
@@ -0,0 +1,13 @@
+# QuestBasicKotlin_0065
+
+Project tugas Basic Kotlin.
+
+## Identitas
+
+- Nama: Faiz Aditya
+- NIM: [20240140065]
+- Program Studi: Teknologi Informasi
+
+## Materi
+
+Project ini berisi implementasi materi Basic Kotlin dari Pertemuan 2.
---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    println("=== BASIC KOTLIN ===")
+    println("Subbab: WHAT ?")
+    println()
+
+    // Basic output
+    println("Hello, world!")
+
+    // Variables
+    val name = "Faiz Aditya"
+    var age = 20
+
+    println()
+    println("Nama: $name")
+    println("Umur: $age")
+
+    // Mengubah nilai var
+    age = 21
+    println("Umur setelah diubah: $age")
+
+    // String template
+    val customers = 10
+    println("Jumlah customer: $customers")
+}
