d77573e4a943bd6e4c9fec8e6a056b430cbf10ed
Faiz Aditya <faizaditya750@gmail.com>
2026-09-24 20:45:52 +0700

    Collection (List + Set + Map)

--- src/Main.kt
+++ src/Main.kt
@@ -53,5 +53,41 @@ fun main() {
     println("Boolean: $isStudent")
     println("Char: $grade")
     println("String: $university")
+
+        println()
+    println("=== COLLECTION ===")
+
+    // List
+    val readOnlyShapes = listOf("triangle", "square", "circle")
+    println("List: $readOnlyShapes")
+    println("Item pertama: ${readOnlyShapes.first()}")
+    println("Item terakhir: ${readOnlyShapes.last()}")
+    println("Jumlah item: ${readOnlyShapes.count()}")
+    println("Apakah circle ada? ${"circle" in readOnlyShapes}")
+
+    // Mutable List
+    val shapes = mutableListOf("triangle", "square", "circle")
+    shapes.add("rectangle")
+    println("Mutable List setelah ditambah: $shapes")
+    shapes.remove("square")
+    println("Mutable List setelah dihapus: $shapes")
+
+    // Set
+    val fruits = setOf("apple", "banana", "cherry", "cherry")
+    println("Set: $fruits")
+    println("Jumlah item unik: ${fruits.count()}")
+    println("Apakah apple ada? ${"apple" in fruits}")
+
+    // Map
+    val juiceMenu = mapOf(
+        "apple" to 100,
+        "kiwi" to 190,
+        "orange" to 100
+    )
+
+    println("Map: $juiceMenu")
+    println("Harga apple juice: ${juiceMenu["apple"]}")
+    println("Jumlah menu: ${juiceMenu.count()}")
+    println("Apakah apple tersedia? ${juiceMenu.containsKey("apple")}")
 }
-}
+
